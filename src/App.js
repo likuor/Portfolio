17c02ab0ebda37eac1eea5ed60eeff602445468a
Koki Sakai <likuor0528@gmail.com>
2021-12-29 13:21:17 -0800

    created skills and contact modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
 import { Header } from './components/Header';
+import { Skills } from './components/Skills';
+import { Contact } from './components/Contact';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Skills />
+      <Contact />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+export const Contact = () => {
+  return (
+    <div id='contact' className='contact'>
+      <div className='container'>
+        <div className='contact-contents'>
+          <h2>Send me messages</h2>
+          <p className='description'>Feel free to contact me!</p>
+          <a href='https://twitter.com/le_pereza' className='btn'>
+            Go chatting
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
