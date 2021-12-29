0bd19897044228d81bc661d6c53a2cbf6b64423a
Koki Sakai <likuor0528@gmail.com>
2021-12-29 12:51:38 -0800

    installed react-ions. Put icons on Header page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import CoverImage from '../images/coverImage.jpg';
 import ProfileImage from '../images/profileImage.jpg';
+import { FaTwitter, FaGithub, FaInstagram, FaFacebook } from 'react-icons/fa';
 
 export const Header = () => {
   return (
@@ -18,8 +19,21 @@ export const Header = () => {
             <h1 className='title-text'>Koki</h1>
             <h3 className='title-text'>Web Developer</h3>
             <ul className='social-icons'>
-              <li className='icon-link'></li>
-              <li className='icon-link'></li>
+              <li className='icon-link'>
+                <a href='https://www.instagram.com/le_pereza'>
+                  <FaInstagram color='white' size='2rem' />
+                </a>
+              </li>
+              <li className='icon-link'>
+                <a href='https://www.facebook.com/sakai.kouki.1'>
+                  <FaFacebook color='white' size='2rem' />
+                </a>
+                <li className='icon-link'>
+                  <a href='https://github.com/likuor'>
+                    <FaGithub color='white' size='2rem' />
+                  </a>
+                </li>
+              </li>
             </ul>
           </div>
         </div>
