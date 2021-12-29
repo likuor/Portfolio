2a2f21b84f881701713463dee71f8b00842e8bb1
Koki Sakai <likuor0528@gmail.com>
2021-12-29 12:38:36 -0800

    created components directory and fundamental files in it

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import CoverImage from '../images/coverImage.jpg';
+import ProfileImage from '../images/profileImage.jpg';
+
+export const Header = () => {
+  return (
+    <header
+      className='main-cover'
+      style={{ backgroundImage: `url(${CoverImage})` }}
+    >
+      <div className='overlay'></div>
+      <div className='container'>
+        <div className='display-table'>
+          <div className='display-table-contents'>
+            <div
+              className='profile-thumb'
+              style={{ backgroundImage: `url(${ProfileImage})` }}
+            ></div>
+            <h1 className='title-text'>Koki</h1>
+            <h3 className='title-text'>Web Developer</h3>
+            <ul className='social-icons'>
+              <li className='icon-link'></li>
+              <li className='icon-link'></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
