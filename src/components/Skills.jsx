f869e2ea7d0c8d60d768681b41089fee99555321
Koki Sakai <likuor0528@gmail.com>
2022-01-01 23:37:52 -0800

    creating Reducer and implement circle graphs of github date

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,16 +1,31 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios';
+import {
+  skillReducer,
+  initialState,
+  actionTypes,
+} from '../reducers/skillReducer';
+import { requestStates } from '../constants';
+import Circle from 'react-circle';
 
 export const Skills = () => {
-  const [languageList, setLanguageList] = useState([]);
-  console.log(languageList);
+  const [state, dispatch] = useReducer(skillReducer, initialState);
 
   useEffect(() => {
-    axios.get('https://api.github.com/users/likuor/repos').then((response) => {
-      const languageList = response.data.map((res) => res.language);
-      const countedLanguageList = generateLanguageCountObj(languageList);
-      setLanguageList(countedLanguageList);
-    });
+    dispatch({ type: actionTypes.fetch });
+    axios
+      .get('https://api.github.com/users/likuor/repos')
+      .then((response) => {
+        const languageList = response.data.map((res) => res.language);
+        const countedLanguageList = generateLanguageCountObj(languageList);
+        dispatch({
+          type: actionTypes.success,
+          payload: { languageList: countedLanguageList },
+        });
+      })
+      .catch(() => {
+        dispatch({ type: actionTypes.error });
+      });
   }, []);
 
   const generateLanguageCountObj = (allLanguageList) => {
@@ -27,13 +42,44 @@ export const Skills = () => {
     });
   };
 
+  const converseCountToPercentage = (count) => {
+    if (count > 10) {
+      return 100;
+    }
+    return count * 10;
+  };
+
+  const sortedLanguageList = () =>
+    state.languageList.sort(
+      (firstLang, nextLang) => nextLang.count - firstLang.count
+    );
+
   return (
     <div id='Skills'>
       <div className='container'>
         <div className='heading'>
           <h2>Skills</h2>
         </div>
-        <div className='skills-container'></div>
+        <div className='skills-container'>
+          {state.requestState === requestStates.loading && (
+            <p className='description'>Loading...</p>
+          )}
+          {state.requestState === requestStates.success &&
+            sortedLanguageList().map((item, index) => (
+              <div className='skill-item' key={index}>
+                <p className='description'>
+                  <strong>{item.language}</strong>
+                </p>
+                <Circle
+                  animate
+                  progress={converseCountToPercentage(item.count)}
+                />
+              </div>
+            ))}
+          {state.requestState === requestStates.error && (
+            <p className='description'>Error...</p>
+          )}
+        </div>
       </div>
     </div>
   );
