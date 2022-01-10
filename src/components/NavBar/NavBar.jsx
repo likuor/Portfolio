e07d12259d695a70399921e7dd0b7d90f06b220f
Koki Sakai <likuor0528@gmail.com>
2022-01-09 17:44:50 -0800

    modifying navbar to do responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,18 +4,24 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-      <ul>
-        <li>
+    <div className='nav-wrapper'>
+      <h1 className='nav-logo'>
+        Koki Sakai <i className='fab fa-react'></i>
+      </h1>
+      <div className='menu-icon'>
+        <i className='fas fa-times'></i>
+      </div>
+      <ul className='nav-menu'>
+        <li className='nav-links'>
           <NavLink to='/'>Home</NavLink>
         </li>
-        <li>
+        <li className='nav-links'>
           <NavLink to='/about'>About</NavLink>
         </li>
-        <li>
+        <li className='nav-links'>
           <NavLink to='/works'>Works</NavLink>
         </li>
-        <li>
+        <li className='nav-links'>
           <NavLink to='/contact'>Contact</NavLink>
         </li>
       </ul>
