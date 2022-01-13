0d5fbc70718150e065db7087f4f8120d3e807b10
Koki Sakai <likuor0528@gmail.com>
2022-01-12 23:06:20 -0800

    Modified Home page css and navbar css

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
     <section>
-      <div>
-        <div className='main-wrapper'>
-          <h1 className='intro-title'>Hi, I'm Koki Sakai,</h1>
-          <h1 className='intro-title'>web developer</h1>
+      <div className='main-wrapper'>
+        <div className='intro-title'>
+          <h1 className='title-h1'>Hi,</h1>
+          <h1 className='title-h1'>I'm Koki Sakai,</h1>
+          <h1 className='title-h1'>web developer</h1>
         </div>
       </div>
     </section>
