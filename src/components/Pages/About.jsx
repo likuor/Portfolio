21fd58f52aff92fcec976c23d69c0b842f237ffd
Koki Sakai <likuor0528@gmail.com>
2022-02-08 15:43:36 -0800

    Modified Profile size for responsive

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
               in Vancouver, Canada.
             </p>
             <div className={styles.imgContainer}>
-              <img src={Icon} alt='ProfileImage' />
+              <img src={Icon} alt='ProfileImage' className={styles.imgSize} />
               <div className={styles.contentsBtn}>
                 <a
                   href='https://drive.google.com/file/d/1xoy35zCP6fVfVwsUAzQrL3SGAiXtQ2LS/view?usp=sharing'
