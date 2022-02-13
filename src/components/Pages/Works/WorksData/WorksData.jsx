703bd653d60e2f23908ee1033d595f49bde15ff1
Koki Sakai <likuor0528@gmail.com>
2022-02-12 18:33:45 -0800

    Add other works in Works page. Modified colum display

--- src/components/Pages/Works/WorksData/WorksData.jsx
+++ src/components/Pages/Works/WorksData/WorksData.jsx
@@ -1,8 +1,13 @@
-import WorksImg from '../WorksImg/Restarea.jpg';
+import WorksClick from '../WorksImg/Restarea.jpg';
+import WorksHomePage from '../WorksImg/Homepage.jpg';
 
 export const WorksData = [
   {
     link: 'https://share.click.gmbh/f1a1c182-89a2-44d8-920f-1da98e468eb8?locale=ja',
-    img: WorksImg,
+    img: WorksClick,
+  },
+  {
+    link: 'https://kokisakai.com/',
+    img: WorksHomePage,
   },
 ];
