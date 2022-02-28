86e0d5c98fdf1a288e64856c446109edd870cc6d
Koki Sakai <likuor0528@gmail.com>
2022-02-27 22:44:38 -0800

    Added googlmap. Modified CSS in Contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,17 +1,23 @@
 import Title from '../../components/Texts/Title';
 import Caption from '../../components/Texts/Caption';
 import Button from '../../components/Button/Button';
-import styles from '../Layout.module.css';
+import styles from './Contact.module.css';
+import Map from '../../components/Map/Map';
 
 const Contact = (props) => {
-  const { sectionWrapper, contentsWrapper } = styles;
+  const { sectionWrapper, contentsWrapper, eachContentsWrapper } = styles;
 
   return (
     <section id={props.id} className={sectionWrapper}>
       <div className={contentsWrapper}>
-        <Title title='Get in touch' />
-        <Caption caption="&lt;p&gt; Don't hesitate to DM! &lt;/p&gt;" />
-        <Button message='Welcome' url='https://twitter.com/le_pereza' />
+        <div className={eachContentsWrapper}>
+          <Title title='Get in touch' />
+          <Caption caption="&lt;p&gt; Don't hesitate to DM! &lt;/p&gt;" />
+          <Button message='Welcome' url='https://twitter.com/le_pereza' />
+        </div>
+        <div className={eachContentsWrapper}>
+          <Map />
+        </div>
       </div>
     </section>
   );
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,30 @@
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const Map = () => {
+  const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+
+  const containerStyle = {
+    height: '70vh',
+    width: '100%',
+  };
+
+  const center = {
+    lat: 49.281766034355286,
+    lng: -123.11977167855632,
+  };
+
+  const positionCanada = {
+    lat: 49.281766034355286,
+    lng: -123.11977167855632,
+  };
+
+  return (
+    <LoadScript googleMapsApiKey={API_KEY}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={14}>
+        <Marker position={positionCanada} />
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default Map;
